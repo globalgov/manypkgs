6504188fbb5ae5246f17303611387c849cefcb9b
Hsposito <henrique.sposito@graduateinstitute.ch>
2021-05-21 14:32:14 +0200

    Fixed minor issues with testing of code_agreements()

--- tests/testthat/test_code_agreements.R
+++ tests/testthat/test_code_agreements.R
@@ -72,14 +72,9 @@ data4 <- data.frame(title = c("Protocol On Amendments To The Agreement On Cooper
                               "Amendments To The Schedule To The International Convention For The Regulation Of Whaling, 20-1 Meeting"),
                     date = c("2015-10-30", "2019-02-01", "1984-10-30", "1955-10-14", "1960-02-05", "1969-06-27"))
 
-<<<<<<< HEAD
 test_that("code_agreements() does not link treaties that are not agreements", {
-  expect_equal(code_agreements(data4$title, data4$date), c("20151030P", "20190201E", "19841030P", "19551014P", "19690627E2", "19600205A"))
-=======
-test_that("code_agreements() link treaties correctly", {
   expect_equal(code_agreements(data4$title, data4$date), c("20151030P", "20190201E", "19841030P", "19551014P", "19600205A", "19690627E2_19600205A"))
 })
-
 
 data5 <- data.frame(title = c("Amendments On The Transport Of Corrosive Substances To Protocol 18 Of The 1868 Revised Convention On The Navigation Of The Rhine",
                               "Amendments Of The Limitation Amounts In The 1992 Protocol To Amend The International Convention On Civil Liability For Oil Pollution Damage",
@@ -88,7 +83,6 @@ data5 <- data.frame(title = c("Amendments On The Transport Of Corrosive Substanc
                     date = c("1899-10-02", "2000-10-18", "1940-11-07", "1999-03-02"))
 
 test_that("code_agreements() recognizes the correct type of treaty", {
-  expect_equal(code_agreements(data5$title, data5$date), c("18991002E18", "20001018E_CLC19691129", "19401107X", "P19990302"))
->>>>>>> 5f6eb7054f66ef5c8b1ca92834f11d75a5e25951
+  expect_equal(code_agreements(data5$title, data5$date), c("18991002E18", "20001018E_CLC19691129", "19401107X", "19990302P"))
 })
 
