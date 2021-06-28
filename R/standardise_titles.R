d88e668024452caaf032ef3fa2cf7c786f9f8f7d
Hsposito <henrique.sposito@graduateinstitute.ch>
2021-06-28 11:41:54 +0200

    Minor updates to standardise_titles() to fix issue with how special character is replaced

--- R/standardise_titles.R
+++ R/standardise_titles.R
@@ -97,7 +97,7 @@ standardise_titles <- standardize_titles <- function(s, strict = FALSE, api_key 
   # Delete hyphens when separating two parts of the title (when there is a space before and after)
   out <- gsub(" - ", " ", out)
   # Delete special character found in some treaty titles
-  out <- gsub("¬í", " ", out)
+  out <- gsub("¬[[:alpha:]]{1}", " ", out)
   # Add space after a comma
   out <- textclean::add_comma_space(out)
   
