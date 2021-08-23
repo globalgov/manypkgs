b3d46481e327cbdd94f92c5906956eed36fe8eff
EstherPeev <esther.peev@graduateinstitute.ch>
2021-08-23 15:31:44 +0200

    Updated code_term_type() with the table of termination type classification appearing when the function is run without argument

--- R/code_term.R
+++ R/code_term.R
@@ -39,13 +39,25 @@ code_term_date <- function(title, text = NULL) {
 #' GNEVAR$Term_Type <- code_term_type(GNEVAR$Title)
 #' @export
 code_term_type <- function(title, text = NULL){
+  
+  if (missing(title)) {
+    # If missing argument, function returns list of termination clause types
+    type <- as_tibble(termination_type)
+    type$meaning[7] <- paste(substr(type$meaning[7], 0, 100), "...")
+    type$meaning[10] <- paste(substr(type$meaning[10], 0, 100), "...")
+    type$meaning[11] <- paste(substr(type$meaning[11], 0, 100), "...")
+    type$meaning[12] <- paste(substr(type$meaning[12], 0, 100), "...")
+    type <- knitr::kable(type, "simple", caption = "Treaty Termination Type, source: https://www.srdlawnotes.com/2017/08/termination-of-treaties.html")
+    type
+  }  else {
   # Step one: if the term date is mentioned in the treaty title,
   # it should be classified as "Sunset/Expiry"
   title <- as.character(title)
   # All the treaties having the term date in the title had the
   # word "For the Period"
-  type <- ifelse(grepl("For The Period", title) & grepl("\\d{1,2}.\\w{3,}.\\d{4} To \\d{1,2}.\\w{3,}.\\d{4}", title, ignore.case = T), paste0("Sunset/Expiry"), NA)
+  type <- ifelse(grepl("For The Period", title) & grepl("\\d{1,2}.\\w{3,}.\\d{4} To \\d{1,2}.\\w{3,}.\\d{4}", title, ignore.case = T), paste0("EXP"), NA)
   type
   # TO DO: Step two: use treaty text to identify which termination
   # clause type the treaty has.
+  }
 }
