55136ca5a84583df1992ae5bc1957dc7e9243d08
EstherPeev <esther.peev@graduateinstitute.ch>
2021-09-01 18:19:28 +0200

    Deleted example in get_package_name() and the tests

--- tests/testthat/test_export_data.R
+++ tests/testthat/test_export_data.R
@@ -5,7 +5,3 @@ test_that("url is declared", {
 test_that("url is valid", {
   expect_error(export_data("COW", databse = "states", URL = 5), "Please provide a valid URL argument.")
 })
-
-test_that("Package name is correct", {
-  expect_equal(get_package_name(), "qCreate")
-})
