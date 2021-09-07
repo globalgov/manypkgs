b9a232ee9dd44fed14ce1785e9e2590ad31204a0
Hsposito <henrique.sposito@graduateinstitute.ch>
2021-09-07 10:12:52 +0200

    Updated tests for condense_qID() function

--- tests/testthat/test_code_agreements.R
+++ tests/testthat/test_code_agreements.R
@@ -70,7 +70,6 @@ test_that("certain functions return coding information when argument is missing"
   expect_type(code_type(), "character")
   expect_type(code_known_agreements(), "character")
   expect_type(code_activity(), "character")
-  expect_type(pred_words(), "character")
 })
 
 # Test that the punctuation marks are not in the qID
