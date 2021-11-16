1ba73219cbd75ce7b32d0e4494247d45cd540485
EstherPeev <esther.peev@graduateinstitute.ch>
2021-11-16 15:26:27 +0100

    Changed get_memberships() function so it returns a dataframe and added the option of having only one database argument

--- R/memberships.R
+++ R/memberships.R
@@ -1,22 +1,44 @@
 #' Get memberships' list
 #'
-#' @param country A country variable
-#' @param dataset_id A treaty ID variable
+#' Memberships database have countries column and 
+#' treaty title column but information on the other
+#' countries that are part to the treaties is often
+#' missing.
+#' The function allows to generate a dataframe of 
+#' treaty IDs and the countries part to the treaty.
+#' @param actor A country variable
+#' @param id A treaty ID variable
 #'
-#' @return A list of countries part of the treaty
+#' @return A dataframe of treaty IDs and countries
+#' part of the treaty
 #' @import dplyr
 #' @examples
-#' IEADB_MEM$Memberships <- get_memberships(IEADB_MEM$CountryID, IEADB_MEM$IEADB_ID)
+#' sample <- qEnviron::memberships$IEADB_MEM
+#' get_memberships(actor = sample$CountryID, id = sample$qID_ref)
+#' get_memberships(qEnviron::memberships)
 #' @export
-get_memberships <- function(country, dataset_id){
-  country <- as.character(country)
-  dataset_id <- as.character(dataset_id)
-  s <- cbind(country, dataset_id)
+get_memberships <- function(database, actor, id){
+  if (!missing(database)) {
+    id <- unname(unlist(purrr::map(database, "qID_ref")))
+    actor <- unname(unlist(purrr::map(database, "CountryID")))
+    s <- cbind(actor, id)
+    s <- as.data.frame(s)
+    k <- s %>% 
+      dplyr::group_by(id) %>% dplyr::summarise(Memberships = toString(actor)) %>% 
+      dplyr::ungroup()
+    s <- dplyr::left_join(s, k, by = "id") %>% 
+      dplyr::select(id, Memberships) %>% unique()
+    s
+  }else {
+  actor <- as.character(actor)
+  id <- as.character(id)
+  s <- cbind(actor, id)
   s <- as.data.frame(s)
   k <- s %>% 
-    dplyr::group_by(dataset_id) %>% dplyr::summarise(Memberships = toString(country)) %>% 
+    dplyr::group_by(id) %>% dplyr::summarise(Memberships = toString(actor)) %>% 
     dplyr::ungroup()
-  s <- dplyr::left_join(s, k, by = "dataset_id")
-  s <- s$Memberships
+  s <- dplyr::left_join(s, k, by = "id") %>% 
+    dplyr::select(id, Memberships)
   s
+  }
 }
