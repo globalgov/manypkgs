cd7ce0bc00a4ddfa6ea58a3a258a90be40d5d525
Hsposito <henrique.sposito@graduateinstitute.ch>
2021-12-08 12:23:04 +0100

    Renamed script and updated tests for get_articles()

---
+++ tests/testthat/test_get_articles.R
@@ -0,0 +1,13 @@
+treaty <- "preamble: this is the preamble \narticle 1: this is the article 1 
+\narticle 2: this is the article 2 \narticle 3: open for accession \narticle 4: shall terminate as"
+
+test_that("Treaty text is splitted correctly", {
+  t <- get_articles(treaty)
+  expect_length(t, 1)
+  expect_equal(lengths(t), 5)
+  expect_equal(get_articles(treaty, article = "preamble"), "preamble: this is the preamble ")
+  expect_equal(get_articles(treaty, article = "memberships"), list(" 3: open for accession "))
+  expect_equal(get_articles(treaty, article = "termination"), list(" 4: shall terminate as"))
+  expect_equal(get_articles(treaty, article = 1), " 1: this is the article 1 \n")
+  expect_equal(get_articles(treaty, match = "preamble"), list("preamble: this is the preamble "))
+})
