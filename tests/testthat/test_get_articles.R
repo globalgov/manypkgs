215722ff38ba52445912407f669ca064900eb816
EstherPeev <esther.peev@graduateinstitute.ch>
2021-12-20 18:36:33 +0100

    Updated test for get_articles() with new example of clause

--- tests/testthat/test_get_articles.R
+++ tests/testthat/test_get_articles.R
@@ -1,5 +1,5 @@
 treaty <- "preamble: this is the preamble \narticle 1: this is the article 1 
-\narticle 2: this is the article 2 \narticle 3: open for accession \narticle 4: shall terminate as"
+\narticle 2: this is the article 2 \narticle 3: open for accession \narticle 4: shall terminate as \n shall be dissolved"
 
 test_that("Treaty text is splitted correctly", {
   t <- get_articles(treaty)
@@ -7,7 +7,7 @@ test_that("Treaty text is splitted correctly", {
   expect_equal(lengths(t), 5)
   expect_equal(get_articles(treaty, article = "preamble"), "preamble: this is the preamble ")
   expect_equal(get_articles(treaty, article = "memberships"), list(" 3: open for accession "))
-  expect_equal(get_articles(treaty, article = "termination"), list(" 4: shall terminate as"))
+  expect_equal(get_articles(treaty, article = "termination"), list(" 4: shall terminate as \n shall be dissolved"))
   expect_equal(get_articles(treaty, article = 1), " 1: this is the article 1 \n")
   expect_equal(get_articles(treaty, match = "preamble"), list("preamble: this is the preamble "))
 })
