bb2da9a57d0ed46f15eee3c65d767b5cc1742c00
EstherPeev <esther.peev@graduateinstitute.ch>
2021-12-20 14:41:04 +0100

    Arrange tests for termination and memberships function

--- tests/testthat/test_termination.R
+++ tests/testthat/test_termination.R
@@ -1,3 +1,4 @@
+# Test for code_grounds()
 data <- data.frame(title = c("Treaty For The Period From 1 September 1992 To 18 April 1998 Between France And Spain",
                              "Treaty Between France And Italy For The Period Between The 1 September 1992 To 18 April 1998",
                              "Treaty Signed in Berlin 8 August 1998 Between Germany and Bulgaria",
@@ -9,7 +10,6 @@ data <- data.frame(title = c("Treaty For The Period From 1 September 1992 To 18 
 
 # Test term type is detected correctly from treaty title
 test_that("treaty type is identified from treaty title", {
-  # Add title to the code_dates function arguments
   expect_equal(code_grounds(data$title),
                c("EXP", "EXP", NA, "EXP", NA, "EXP", "EXP"))
 })
@@ -28,6 +28,7 @@ data2 <- data.frame(title = c("Amendments On The Transport Of Corrosive Substanc
                             communicated to the ministry of foreign relations of el salvador. the duration shall be prolonged automatically and 
                             tacitly so long as the convention continues in effect for four or more of the signatory countries."))
 
+# Termination types identified through treaty texts
 test_that("treaty termination is identified from treaty text", {
   expect_equal(code_grounds(data2$title, data2$text),
                c("EXP", "SUB", "EXP"))
@@ -37,4 +38,24 @@ test_that("treaty termination is identified from treaty text", {
 # Test that table appears if no argument is mentioned
 test_that("function returns information when no argument is mentioned", {
   expect_type(code_grounds(), "character")
+})
+
+# Test for code_terms()
+data <- data.frame(title = c("Treaty For The Period From 1 September 1992 To 18 April 1998 Between France And Spain",
+                             "Treaty Between France And Italy For The Period Between The 1 September 1992 To 18 April 1998",
+                             "Treaty Signed in Berlin 8 August 1998 Between Germany and Bulgaria",
+                             "Treaty Concerning Geneva Lake For The Period 1 September 1992 To 18 April 1999",
+                             "Protocol To The 17 May 1998 Convention on Fisheries",
+                             "Treaty For The Period Between 22 February 2002 To 12 March 2009 Concerning Jan Mayen Seal Fishery",
+                             "Amendments For The Period 19 May 1992 To 18 September 1998 Convention For The Prevention Of Pollution From Ships"),
+                   date = c("1783-10-03", "1783-10-03", "1876-10-28", "1876-10-28", "1982-12-10", "1876-10-28", "1876-10-28"))
+
+# Test term date is correctly identified from treaty title
+test_that("treaty term date is identified from treaty title", {
+  expect_equal(code_term(data$title),
+               as.Date(c("1998-04-18",
+                         "1998-04-18",
+                         NA, "1999-04-18",
+                         NA, "2009-03-12",
+                         "1998-09-18")))
 })
