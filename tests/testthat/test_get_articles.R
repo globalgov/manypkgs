c93d8368658716b1364883c4ea1737c3f6d93bc2
Hsposito <henrique.sposito@graduateinstitute.ch>
2022-01-10 16:56:11 +0100

    Updated and simplified tests for get_articles()

--- tests/testthat/test_get_articles.R
+++ tests/testthat/test_get_articles.R
@@ -1,21 +1,16 @@
 treaty <- "preamble: this is the preamble \narticle 1: this is the article 1 
-\narticle 2: this is the article 2 \narticle 3: open for accession \narticle 4: shall terminate as \n shall be dissolved"
+\narticle 2: this is the article 2 \narticle 3: open for accession
+\narticle 4: shall terminate as \n shall be dissolved
+\nannex: this is an annex"
 
 test_that("Treaty text is splitted correctly", {
   t <- get_articles(treaty)
   expect_length(t, 1)
-  expect_equal(lengths(t), 5)
-  expect_equal(get_articles(treaty, article = "preamble"), "preamble: this is the preamble ")
-  expect_equal(get_articles(treaty, article = "memberships"), list(" 3: open for accession "))
-  expect_equal(get_articles(treaty, article = "termination"), list(" 4: shall terminate as \n shall be dissolved"))
-  expect_equal(get_articles(treaty, article = 1), " 1: this is the article 1 \n")
-  expect_equal(get_articles(treaty, match = "preamble"), list("preamble: this is the preamble "))
-})
-
-# Test that the helper function split_treaty() works on annexes
-treaty2 <- "preamble: this is the preamble \narticle 1: this is the article 1 
-\narticle 2: this is the article 2 \narticle 3: open for accession \nannex I: supplementary informations"
-
-test_that("Treaty are divided by articles and annex", {
-  expect_equal(get_articles(treaty2, "annex"), as.list("∑ i: supplementary informations"))
+  expect_equal(lengths(t), 6)
+  expect_equal(get_articles(treaty, article = "preamble"), "preamble: this is the preamble")
+  expect_equal(get_articles(treaty, article = "memberships"), list("3: open for accession"))
+  expect_equal(get_articles(treaty, article = "termination"), list("4: shall terminate as \n shall be dissolved"))
+  expect_equal(get_articles(treaty, article = "annex"), list("annex: this is an annex"))
+  expect_equal(get_articles(treaty, article = 1), "1: this is the article 1")
+  expect_equal(get_articles(treaty, match = "preamble"), list("preamble: this is the preamble"))
 })
