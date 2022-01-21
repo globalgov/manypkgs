098c1471679b5277e6dfb3e30f913751524575dd
Hsposito <henrique.sposito@graduateinstitute.ch>
2022-01-21 15:55:05 +0100

    Updated tests for new function names

--- tests/testthat/test_termination.R
+++ tests/testthat/test_termination.R
@@ -1,4 +1,4 @@
-# Test for code_grounds()
+# Test for code_term()
 data <- data.frame(title = c("Treaty For The Period From 1 September 1992 To 18 April 1998 Between France And Spain",
                              "Treaty Between France And Italy For The Period Between The 1 September 1992 To 18 April 1998",
                              "Treaty Signed in Berlin 8 August 1998 Between Germany and Bulgaria",
@@ -8,9 +8,9 @@ data <- data.frame(title = c("Treaty For The Period From 1 September 1992 To 18 
                              "Amendments For The Period 19 May 1992 To 18 September 1998 Convention For The Prevention Of Pollution From Ships"),
                    date = c("1783-10-03", "1783-10-03", "1876-10-28", "1876-10-28", "1982-12-10", "1876-10-28", "1876-10-28"))
 
-# Test term type is detected correctly from treaty title
+# Test term_date type is detected correctly from treaty title
 test_that("treaty type is identified from treaty title", {
-  expect_equal(code_grounds(data$title),
+  expect_equal(code_term(data$title),
                c("EXP", "EXP", NA, "EXP", NA, "EXP", "EXP"))
 })
 
@@ -26,7 +26,7 @@ data2 <- data.frame(title = c("Amendments On The Transport Of Corrosive Substanc
                              "Treaty to preserve the lake of Geneva"),
                    text = c("the present convention shall remain in force for a period of five years and thereafter until two years 
                             from the date when either of the high contracting parties shall give notice to the other of its desire to 
-                            terminate it",
+                            term_dateinate it",
                             "this convention shall, from the date of the exchange of ratifications be deemed to supplant the 
                             convention between his britannic majesty and the united states of america for the preservation of the halibut 
                             fishery of the northern pacific ocean including bering sea, concluded march 2, 1923",
@@ -35,26 +35,26 @@ data2 <- data.frame(title = c("Amendments On The Transport Of Corrosive Substanc
                             communicated to the ministry of foreign relations of el salvador. the duration shall be prolonged automatically and 
                             tacitly so long as the convention continues in effect for four or more of the signatory countries.",
                             "Any member states may withdraw from this present convention",
-                            "This agreement shall terminate upon the complete completion of the project",
+                            "This agreement shall term_dateinate upon the complete completion of the project",
                             "Member state can renounce its memberships at any time after ratification",
                             "In the cases of extraordinary events, members states can withdrawal",
                             "In the case of war, the parties to the treaties will end this agreement",
                             "If one become party injurious, the treaty come to an end and parties are liberate from their obligations",
                             "Nonperformance of obligations will generate the end of the treaty"))
 
-# Termination types identified through treaty texts
-test_that("treaty termination is identified from treaty text", {
-  expect_equal(code_grounds(data2$title, data2$text),
-               c("SUN", "SUB", "SUN", "WTH", "SUC", "REN", "REB", "WAR", "INJ", "NON"))
+# term_dateination types identified through treaty texts
+test_that("treaty term is identified from treaty text", {
+  expect_equal(code_term(data2$title, data2$text),
+               c("SUN", "SUB", "SUN", "WTH", "NA", "REN", "REB", "WAR", "INJ", "NON"))
 })
 
 
 # Test that table appears if no argument is mentioned
 test_that("function returns information when no argument is mentioned", {
-  expect_type(code_grounds(), "character")
+  expect_type(code_term(), "character")
 })
 
-# Test for code_terms()
+# Test for code_term_date()
 data <- data.frame(title = c("Treaty For The Period From 1 September 1992 To 18 April 1998 Between France And Spain",
                              "Treaty Between France And Italy For The Period Between The 1 September 1992 To 18 April 1998",
                              "Treaty Signed in Berlin 8 August 1998 Between Germany and Bulgaria",
@@ -65,12 +65,11 @@ data <- data.frame(title = c("Treaty For The Period From 1 September 1992 To 18 
                    date = c("1783-10-03", "1783-10-03", "1876-10-28", "1876-10-28", "1982-12-10", "1876-10-28", "1876-10-28"))
 
 # Test term date is correctly identified from treaty title
-test_that("treaty term date is identified from treaty title", {
-  expect_equal(code_term(data$title),
+test_that("treaty term_date date is identified from treaty title", {
+  expect_equal(code_term_date(data$title),
                as.Date(c("1998-04-18",
                          "1998-04-18",
                          NA, "1999-04-18",
                          NA, "2009-03-12",
                          "1998-09-18")))
 })
-
