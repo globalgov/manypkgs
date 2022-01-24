7cd13553510f2f1f96b2f0e6404327b5b50fb1e0
Hsposito <henrique.sposito@graduateinstitute.ch>
2022-01-24 15:17:45 +0100

    Updated tests for code_lineage()

--- tests/testthat/test_code_lineage.R
+++ tests/testthat/test_code_lineage.R
@@ -14,7 +14,7 @@ data <- tibble::tibble(title = c("Agreement Between Cape Verde And Portugal On F
 test_that("code_lineage function works", {
   expect_equal(code_lineage(title = data$title), c("CPV-PRT[FSD] - fishing", "NA - environment",
                                                    "NA - territorial boundaries", "River",
-                                                   "NA - trade and investment",
+                                                   "NA - trade",
                                                    "River Basin", "NA - environment",
                                                    "RUS-USA[KTC] - fishing", "Pacific Ocean - fishing"))
 })
