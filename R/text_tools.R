ed90551f7de0c4a9bc994df87f32ba3514ecea7c
Hsposito <henrique.sposito@graduateinstitute.ch>
2022-02-24 10:50:01 +0100

    Added first draft for funtion that converts readable and non-readable PDFs into text

---
+++ R/text_tools.R
@@ -0,0 +1,24 @@
+#' Convert treaty PDFs to text
+#'
+#' @param path The file path.
+#' The path can also be specified to a folder with multiple PDF files,
+#' in this case all PDF files in the folder will be converted.
+#' @return A list with the converted texts.
+#' @importFrom pdftools pdf_text pdf_ocr_text
+#' @source https://stackoverflow.com/questions/71064939/how-to-check-if-pdf-is-scanned-image-or-contains-text-in-r
+#' @export
+treaty_pdf <- function(path) {
+  files <- list.files(path, pattern = ".pdf$")
+  out <- lapply(files, function(f) {
+    out <- pdftools::pdf_text(f)
+    contains_text <- nchar(out) > 15
+    out <- ifelse(!contains_text, pdftools::pdf_ocr_text(f), out)
+    out <- data.frame(text = out)
+  })
+  out <- out %>%
+   dplyr::bind_rows() %>%
+   trimws(which = "both")
+}
+
+path = getwd()
+treaty_pdf(path)
