ca25957829aae7682bd34164e4bc17046aa938fd
Henrique Sposito <henrique.sposito@graduateinstitute.ch>
2022-03-18 11:40:46 +0100

    Updated treaty_pdf() function

--- R/text_tools.R
+++ R/text_tools.R
@@ -1,18 +1,30 @@
-#' Convert treaty PDFs to text
+#' Convert PDFs to text
 #'
-#' @param path The file path.
+#' @param path The file(s) path.
 #' The path can also be specified to a folder with multiple PDF files,
 #' in this case all PDF files in the folder will be converted.
 #' @return A list with the converted texts.
 #' @importFrom pdftools pdf_text pdf_ocr_text
+#' @importFrom stringi stri_trans_general
 #' @source https://stackoverflow.com/questions/71064939/how-to-check-if-pdf-is-scanned-image-or-contains-text-in-r
+#' @examples
+#' \dontrun{
+#' treaty_pdf(path = getwd())
+#' }
 #' @export
 treaty_pdf <- function(path) {
+  # get all PDFs
   files <- list.files(path, pattern = ".pdf$")
-  out <- lapply(files, function(f) {
-    out <- pdftools::pdf_text(f)
-    contains_text <- nchar(out) > 15
-    out <- ifelse(!contains_text, pdftools::pdf_ocr_text(f), out)
+  # transform to text if readable, or ocr if not
+  pdfs <- lapply(files, function(f) {
+    pdfs <- pdftools::pdf_text(f)
+    contains_text <- nchar(pdfs) > 15
+    pdfs <- ifelse(!contains_text, pdftools::pdf_ocr_text(pdfs), pdfs)
+  })
+  # unlist and do some pre-processing
+  out <- lapply(pdfs, function(g) {
+    out <- unlist(g)
+    out <- stringi::stri_trans_general(out, "latin-ascii")
   })
   out
 }
