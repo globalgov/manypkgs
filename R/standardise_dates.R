7b9c33d70d81219ea6438e6b4ce3cb19bceb6178
Henrique Sposito <henrique.sposito@graduateinstitute.ch>
2022-04-29 14:38:52 +0200

    Added interactive argument to stamdardise_dates() function that allows users to manually select how 6 digit ambigous dates are to be ordered

--- R/standardise_dates.R
+++ R/standardise_dates.R
@@ -9,6 +9,9 @@
 #' into a range of dates.
 #' @name standardise_dates
 #' @param ... One (ymd) or three (yyyy, mm, dd) variables
+#' @param interactive By default FALSE.
+#' If TRUE, it allows users to choose the correct order for
+#' ambiguous 6 digit date in a messydt vector.
 #' @details The function seeks to convert a wide range of dates into
 #' dates so that these can be meaningfully used for analysis.
 #' There are several limitations of other date wrangling packages
@@ -19,6 +22,8 @@
 #' range of dates.
 #' @return Nested vector of dates under messydt class
 #' @importFrom messydates make_messydate as_messydate
+#' @importFrom stringr str_detect
+#' @importFrom utils menu
 #' @examples
 #' library(tibble)
 #' dates_comparison <- tibble::tribble(~Example, ~OriginalDate,
@@ -34,15 +39,43 @@
 #' manypkgs = manypkgs::standardise_dates(OriginalDate)
 #' ) %>% print(n = 25)
 #' @export
-standardise_dates <- function(...) {
+standardise_dates <- function(..., interactive = FALSE) {
   dots <- list(...)
   if (length(dots) == 1) {
     x <- messydates::as_messydate(...)
   } else if (length(dots) == 3) {
     x <- messydates::make_messydate(...)
   }
+  if (isTRUE(interactive)) {
+    x <- ifelse(stringr::str_detect(x, "^([:digit:]{2})-([:digit:]{2})-([:digit:]{2}$)") &
+                  as.numeric(gsub("-", "", stringr::str_extract(x, "^[:digit:]{2}-"))) < 32 &
+                  as.numeric(gsub("-", "", stringr::str_extract(x, "-[:digit:]{2}-"))) < 32 &
+                  as.numeric(gsub("-", "", stringr::str_extract(x, "[:digit:]{2}$"))) < 32, ask_user(x), x)
+    x <- messydates::as_messydate(x)
+    }
+    x
 }
 
 #' @rdname standardise_dates
 #' @export
 standardize_dates <- standardise_dates
+
+ask_user <- function(d) {
+  input <- menu(c("Year-Month-Day", "Day-Month-Year", "Month-Day-Year"),
+                title = paste0("What format is this date ", d," ?"))
+  if (input == 1) {
+    out = d
+    message("Dates already in standard YMD format")
+  }
+  if (input == 2) {
+    out <- stringr::str_replace_all(d, "^([:digit:]{2})-([:digit:]{2})-([:digit:]{2})$",
+                                    "\\3-\\2-\\1")
+    message("Ambiguous 6 digit dates have been changed to standard YMD format")
+  }
+  if (input == 3) {
+    out <- stringr::str_replace_all(d, "^([:digit:]{2})-([:digit:]{2})-([:digit:]{2})$",
+                                    "\\3-\\1-\\2")
+    message("Ambiguous 6 digit dates have been changed to standard YMD format")
+  }
+  out
+}
