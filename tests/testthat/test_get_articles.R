9e855aa8910c07e664c283001c4edc0c9054f73d
Henrique Sposito <henrique.sposito@graduateinstitute.ch>
2022-06-13 12:34:31 +0200

    Updated tests for get_articles() for new changes

--- tests/testthat/test_get_articles.R
+++ tests/testthat/test_get_articles.R
@@ -5,16 +5,17 @@ treaty <- list("preamble: this is the preamble \narticle 1: this is the article 
 \narticle 2: this is the article of an agreement")
 
 test_that("Treaty text is splitted correctly", {
+  treaty <- standardise_texts(treaty)
   expect_length(get_articles(treaty), 2)
   expect_equal(get_articles(treaty, article = "preamble"), list("preamble: this is the preamble ",
                                                                 "preamble: this is the preamble "))
-  expect_equal(get_articles(treaty, article = "memberships"), list(" 3: open for accession ", "NA"))
+  expect_equal(get_articles(treaty, article = "memberships"), list("ARTICLE 3: open for accession ", "NA"))
   expect_equal(get_articles(treaty, article = "termination"),
-               list(" 4: shall terminate as shall be dissolved ", "NA"))
-  expect_equal(get_articles(treaty, article = "annex"), list("annex: this is an annex", "NA"))
+               list("ARTICLE 4: shall terminate as shall be dissolved ", "NA"))
+  expect_equal(get_articles(treaty, article = "annex"), list("ANNEX : this is an annex", "NA"))
   expect_equal(get_articles(treaty, match = "preamble"), list("preamble: this is the preamble ",
                                                               "preamble: this is the preamble "))
   expect_equal(as.character(get_articles(treaty, treaty_type = "agreements")),
-               c("NA", "c(\"preamble: this is the preamble \", \" 1: this is the article 1 \", \" 2: this is the article of an agreement\")"))
+               c("NA", "c(\"preamble: this is the preamble \", \"ARTICLE 1: this is the article 1 \", \"ARTICLE 2: this is the article of an agreement\")"))
   expect_equal(as.character(get_articles(treaty, treaty_type = "amendments")), c("NA", "NA"))
 })
