816559c9e11c8dec317a82508c842b9dd5c1fd77
Henrique Sposito <henrique.sposito@graduateinstitute.ch>
2022-07-14 14:27:55 +0200

    Updated documentation and commented codes in get_pdf() function

--- R/text_tools.R
+++ R/text_tools.R
@@ -39,6 +39,8 @@ standardise_texts <- function(textvar) {
 #' The path can also be specified to a folder with multiple PDF files,
 #' in that case all PDF files in the folder will be converted.
 #' @return A list with the converted texts.
+#' @details If PDF file is not in "readable" format, function attempts to apply
+#' optical character recognition (OCR) to convert these to text.
 #' @importFrom pdftools pdf_text pdf_ocr_text
 #' @importFrom stringi stri_trans_general
 #' @importFrom tm stripWhitespace
@@ -51,13 +53,14 @@ standardise_texts <- function(textvar) {
 get_pdf <- function(path) {
   # get all PDFs
   files <- list.files(path, pattern = ".pdf$")
-  # transform to text if readable, or ocr if not
+  # transform PDF to text if readable, or OCR these if not readable
   pdfs <- lapply(files, function(f) {
     pdfs <- pdftools::pdf_text(f)
+    # check texts not properly converted so that we can try OCR in those
     contains_text <- nchar(pdfs) > 15
     pdfs <- ifelse(!contains_text, pdftools::pdf_ocr_text(pdfs), pdfs)
   })
-  # unlist and do some pre-processing
+  # unlist and do some basic pre-processing
   out <- lapply(pdfs, function(g) {
     out <- paste(g, collapse = " ")
     out <- stringi::stri_trans_general(out, "latin-ascii")
