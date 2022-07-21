da2241184ee1f4f7231c1f4446a6f95c38cf28b0
Henrique Sposito <henrique.sposito@graduateinstitute.ch>
2022-07-21 10:30:12 +0200

    More updates to tests for standardise_treaty_text()

--- tests/testthat/test_standardise_treaty_text.R
+++ tests/testthat/test_standardise_treaty_text.R
@@ -2,14 +2,15 @@ treaty_text <- list("preamble: this is the preamble \narticle 1: this is the art
 \narticle 2: this is the article 2 \narticle 3: open for accession
 \narticle 4: shall terminate as \n shall be dissolved
 \nannex: this is an annex", "preamble: this is the preamble \narticle 1: this is the article 1
-\narticle 2: this is the article of an agreement",
-                    "agreement on trade and commerce <!-- if(window.location==) this page uses javascript//--> agreement between the government of india and the government of bhutan. java script is required -->",
-                    "<[document{the form}")
+\narticle 2: this is the article of an agreement, agreement on trade and commerce
+                    <!-- if(window.location==) this page uses javascript//--> agreement
+                    between the government of india and the government of bhutan.
+                    java script is required -->, <[document{the form}")
 
 test_that("Treaty text is standardised correctly", {
   treaty <- standardise_treaty_text(treaty_text)
   treaty1 <- standardize_treaty_text(treaty_text)
   expect_false(any(stringr::str_detect(treaty, "\\<|\\>|\r|\t|\n")))
-  expect_length(treaty, 4)
+  expect_length(treaty, 2)
   expect_equal(treaty, treaty1)
 })
