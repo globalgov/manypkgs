60a4c410a9c8da965f40c676d687e6d197b8dfc4
Jael Tan <jael.tan@graduateinstitute.ch>
2022-07-21 09:10:24 +0200

    Added text to test

--- tests/testthat/test_standardise_treaty_text.R
+++ tests/testthat/test_standardise_treaty_text.R
@@ -2,7 +2,9 @@ treaty_text <- list("preamble: this is the preamble \narticle 1: this is the art
 \narticle 2: this is the article 2 \narticle 3: open for accession
 \narticle 4: shall terminate as \n shall be dissolved
 \nannex: this is an annex", "preamble: this is the preamble \narticle 1: this is the article 1
-\narticle 2: this is the article of an agreement")
+\narticle 2: this is the article of an agreement",
+                    "agreement between states <!-- if(window.location==) this page uses javascript//-->",
+                    "<[document{the form}")
 
 test_that("Treaty text is standardised correctly", {
   treaty <- standardise_treaty_text(treaty_text)
