468d4c7fe90408c59d9e790fde8a5f4fca4e65d5
Henrique Sposito <henrique.sposito@graduateinstitute.ch>
2022-07-22 17:31:03 +0200

    Renamed `retrieve_clauses()` to `read_clauses()` and moved all other user-facing "retrieve" functions to `{manydata}`

--- tests/testthat/test_read_clauses.R
+++ tests/testthat/test_read_clauses.R
@@ -6,16 +6,16 @@ treaty_text <- list("preamble: this is the preamble \narticle 1: this is the art
 
 test_that("Treaty text is splitted correctly", {
   treaty <- standardise_treaty_text(treaty_text)
-  expect_length(retrieve_clauses(treaty), 2)
-  expect_equal(retrieve_clauses(treaty, article = "preamble"), list("preamble: this is the preamble ",
+  expect_length(read_clauses(treaty), 2)
+  expect_equal(read_clauses(treaty, article = "preamble"), list("preamble: this is the preamble ",
                                                                 "preamble: this is the preamble "))
-  expect_equal(retrieve_clauses(treaty, article = "accession"), list("ARTICLE 3: open for accession ", "NA"))
-  expect_equal(retrieve_clauses(treaty, article = "termination"),
+  expect_equal(read_clauses(treaty, article = "accession"), list("ARTICLE 3: open for accession ", "NA"))
+  expect_equal(read_clauses(treaty, article = "termination"),
                list("ARTICLE 4: shall terminate as shall be dissolved ", "NA"))
-  expect_equal(retrieve_clauses(treaty, article = "annex"), list("ANNEX : this is an annex", "NA"))
-  expect_equal(retrieve_clauses(treaty, match = "preamble"), list("preamble: this is the preamble ",
+  expect_equal(read_clauses(treaty, article = "annex"), list("ANNEX : this is an annex", "NA"))
+  expect_equal(read_clauses(treaty, match = "preamble"), list("preamble: this is the preamble ",
                                                               "preamble: this is the preamble "))
-  expect_equal(as.character(retrieve_clauses(treaty, treaty_type = "agreements")),
+  expect_equal(as.character(read_clauses(treaty, treaty_type = "agreements")),
                c("NA", "c(\"preamble: this is the preamble \", \"ARTICLE 1: this is the article 1 \", \"ARTICLE 2: this is the article of an agreement\")"))
-  expect_equal(as.character(retrieve_clauses(treaty, treaty_type = "amendments")), c("NA", "NA"))
+  expect_equal(as.character(read_clauses(treaty, treaty_type = "amendments")), c("NA", "NA"))
 })
