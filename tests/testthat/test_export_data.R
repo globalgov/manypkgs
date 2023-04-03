532c2e2ab7e1d4f3302ac0ea11cdbf807d9590c6
Henrique Sposito <henrique.sposito@graduateinstitute.ch>
2023-04-03 15:47:57 +0200

    Added tests for the updating of manyIDs

--- tests/testthat/test_export_data.R
+++ tests/testthat/test_export_data.R
@@ -1,9 +1,20 @@
-test_that("url is declared", {
-  expect_error(export_data("COW", databse = "states"),
-               "Please use the URL argument to provide a direct webURL to the source of your dataset.")
-})
-
 test_that("url is valid", {
   expect_error(export_data("COW", databse = "states", URL = 5),
-               "Please provide a valid URL argument.")
+               "Please provide the URL argument to the source of your dataset as a character string.")
+})
+
+a <- data.frame(Title = c("A treaty about things", "treaty about thing"),
+                Beg = c("2010-01-01", "2010-01-01"),
+                treatyID = c("TRTYTH_2010A", "TRTYTH_2010A"),
+                manyID = c("TRTYTH_2010A", "TRTYTH_2010A"))
+b <- data.frame(Title = c("A treaty about things", "treaty about thing"),
+                Beg = c("2010-01-01", "2010-01-01"),
+                treatyID = c("a", "a"),
+                manyID = c("a", "a"))
+ab <- tibble::lst(a, b)
+cd <- tibble::lst(a, a)
+names(cd) <- c("a" ,"b")
+
+test_that("titles, treatyIDs, and manyIDs are correctly updated", {
+  expect_equal(update_ids(ab), update_ids(cd))
 })
