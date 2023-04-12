068414b0c094d3718712015a023014bd35abc409
Jael Tan <jael.tan@graduateinstitute.ch>
2023-04-12 11:20:55 +0200

    Added tests to illustrate issues with linkages in IDs

--- tests/testthat/test-condense_agreements.R
+++ tests/testthat/test-condense_agreements.R
@@ -42,3 +42,70 @@ test_that("Linkages are added correctly", {
   m2 <- merge(data2, a)
   expect_equal(m2, data4)
 })
+
+data5 <- data.frame(treatyID = c("DNK-SWE[FSC]_1899A"),
+                    manyID = c("DNK-SWE[FSC]_1899A"))
+data6 <- data.frame(treatyID = c("DNK-SWE[FSS]_1899A"),
+                    manyID = c("DNK-SWE[FSS]_1899A"))
+data7 <- data.frame(treatyID = c("DNK-SWE[FSC]_1899A",
+                                 "DNK-SWE[FSC]_1902E:DNK-SWE[FSC]_1899A"),
+                    manyID = c("DNK-SWE[FSC]_1899A",
+                               "DNK-SWE[FSC]_1902E:DNK-SWE[FSC]_1899A"))
+test_that("activity is generated correctly", {
+  a <- condense_agreements(idvar = c(data5$treatyID, data6$treatyID))
+  b <- condense_agreements(idvar = c(data5$treatyID, data7$treatyID))
+  j1 <- dplyr::bind_rows(data5, data6)
+  # expect_equal(a$manyID, j1$manyID)
+  j2 <- dplyr::bind_rows(data5, data7)
+  expect_equal(b$manyID, unique(j2$manyID))
+})
+
+data8 <- data.frame(treatyID = c("MEX-TEX[FQG]_1944P",
+                                 "MEX-TEX[FQG]_1944A",
+                                 "MEX-TEX[FQG]_1944P:MEX-TEX[FQG]_1944A"),
+                    manyID = c("MEX-TEX[FQG]_1944P:MEX-TEX[FQG]_1944A",
+                               "MEX-TEX[FQG]_1944A",
+                               "MEX-TEX[FQG]_1944P:MEX-TEX[FQG]_1944A"))
+data9 <- data.frame(treatyID = c("MEX-TEX[FQG]_1944P",
+                                 "MEX-TEX[FQG]_1944A"),
+                    manyID = c("MEX-TEX[FQG]_1944P:MEX-TEX[FQG]_1944A",
+                               "MEX-TEX[FQG]_1944A"))
+data10 <- data.frame(treatyID = c("MEX-TEX[FQG]_1944P",
+                                  "MEX-TEX[FQG]_1944A"),
+                     manyID = c("MEX-TEX[FQG]_1944P",
+                                "MEX-TEX[FQG]_1944A"))
+set1 <- data.frame(title = c("Agreement On Regional Cooperation In Combating Pollution Of The South East Pacific By Oil And Other Harmful Substances In Cases Of Emergency",
+                             "Supplementary Protocol To The Agreement On Regional Cooperation In Combating Pollution Of The South East Pacific By Oil And Other Harmful Substances In Cases Of Emergency"),
+                   date = c("1981-11-12", "1983-07-22"),
+                   treatyID = c("RC09CE_1981A",
+                                "SR10CE_1983P:RC09CE_1981A"),
+                   manyID = c("RC09CE_1981A",
+                              "SR10CE_1983P:RC09CE_1981A"))
+set2 <- data.frame(title = c("Agreement On Regional Cooperation In Combating Pollution Of The South East Pacific By Hydrocarbons And Other Harmful Substances In Cases Of Emergency",
+                             "Supplementary Protocol To The Agreement On Regional Cooperation In Combating Pollution Of The South East Pacific By Oil And Other Harmful Substances In Cases Of Emergency"),
+                   date = c("1981-11-12", "1983-07-22"),
+                   treatyID = c("RC09CE_1981A",
+                                "SR10CE_1983P"),
+                   manyID = c("RC09CE_1981A",
+                              "SR10CE_1983P:RC09CE_1981A"))
+set3 <- data.frame(title = "Agreement On Regional Cooperation In Combating Pollution Of The South East Pacific By Hydrocarbons And Other Harmful Substances In Cases Of Emergency",
+                   date = "1981-11-12",
+                   treatyID = "RC09CE_1981A",
+                   manyID = "RC09CE_1981A")
+test_that("linkage generated from agreements in same dataset", {
+  # scenario 1: manyID generated with linkage when one of the datasets in the database contains both agreements so that treatyID in one dataset is generated with linkage
+  c <- condense_agreements(idvar = c(data8$treatyID, data9$treatyID))
+  d <- condense_agreements(idvar = c(data9$treatyID, data10$treatyID))
+  expect_equal(data8$manyID, c$manyID)
+  # expect_equal(data9$manyID, d$manyID)
+  expect_equal(data10$manyID, d$manyID)
+  # scenario 2: treatyID not generated with linkage due to different titles, but linkage is generated in manyID due to similar treatyIDs
+  idtest1 <- code_agreements(title = set1$title, date = set1$date)
+  idtest2 <- code_agreements(title = set2$title, date = set2$date)
+  expect_equal(idtest1, set1$treatyID)
+  expect_equal(idtest2, set2$treatyID)
+  f <- condense_agreements(idvar = c(set1$treatyID, set2$treatyID))
+  expect_equal(unique(f$manyID), set1$manyID)
+  g <- condense_agreements(idvar = set2$treatyID, set3$treatyID)
+  # expect_equal(g$manyID, set2$manyID)
+})
