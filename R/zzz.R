480f8f121497f9ddd9a6b3dd0160b6f1d28b7bce
Henrique Sposito <henrique.sposito@graduateinstitute.ch>
2023-07-28 17:03:01 +0200

    Fixed spelling mistakes on package startup messages

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 .onAttach <- function(lib, pkg) {
   msg <- c(paste0("manypkgs ", utils::packageVersion("manypkgs")),
-           "\nFor more information about the package pleae visit https://globalgov.github.io/manypkgs/",
+           "\nFor more information about the package please visit https://globalgov.github.io/manypkgs/",
            "\nType 'citation(\"manypkgs\")' for citing this R package in publications.")
   packageStartupMessage(msg)      
   invisible()
--- inst/templates/package-zzz.R
+++ inst/templates/package-zzz.R
@@ -1,6 +1,6 @@
 .onAttach <- function(lib, pkg) {
-  msg <- c(paste0("manypkgs ", utils::packageVersion("{{{package}}}")),
-           "\nFor more information about many packages pleae visit https://manydata.ch/",
+  msg <- c(paste0("{{{package}}} ", utils::packageVersion("{{{package}}}")),
+           "\nFor more information about many packages please visit https://manydata.ch/",
            "\nType 'citation(\"{{{package}}}\")' for citing this R package in publications.")
   packageStartupMessage(msg)      
   invisible()
