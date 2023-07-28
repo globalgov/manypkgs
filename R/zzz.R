48e51685dd3b6113c4d4f6a254dc204caf2f1cb4
Henrique Sposito <henrique.sposito@graduateinstitute.ch>
2023-07-28 15:09:56 +0200

    Added package startup message for package

---
+++ R/zzz.R
@@ -0,0 +1,7 @@
+.onAttach <- function(lib, pkg) {
+  msg <- c(paste0("manypkgs ", utils::packageVersion("manypkgs")),
+           "\nFor more information about the package pleae visit https://globalgov.github.io/manypkgs/",
+           "\nType 'citation(\"manypkgs\")' for citing this R package in publications.")
+  packageStartupMessage(msg)      
+  invisible()
+}
